b48eab8c86a3fd1ac75c6086138b76c8b50ac566
Jess <toxocious@gmail.com>
2023-01-22 19:39:26 -0800

    fix: updated method call for getting field offset in example main.cpp file

--- main.cpp
+++ main.cpp
@@ -24,15 +24,15 @@ DWORD WINAPI Init( HMODULE module )
   const auto image = Aetherim->get_image( "Assembly-CSharp.dll" );
   printf( "\t[Aetherim] Assembly-CSharp -> %s (0x%Ix)\n\n", image->get_name(), reinterpret_cast<uintptr_t>( image ) );
 
-  printf( "[Aetherim] Getting PlayerHandler Class\n" );
+  printf( "[Aetherim] Getting PlayerHandler Class & Fields\n" );
   const auto player = image->get_class( "PlayerHandler" );
   printf( "\t[Aetherim] PlayerHandler -> %s (0x%Ix)\n\n", player->get_name(), reinterpret_cast<uintptr_t>( player ) );
 
-  printf( "[Aetherim] Getting PlayerHandler Fields\n" );
   for ( const auto field : static_cast<Class *>( player )->get_fields() )
   {
     const auto name = Il2cpp::get_field_name( field );
-    printf( "\t[Aetherim] %s (0x%zx)\n", name, player->get_field_offset( name ) );
+
+    printf( "\t[Aetherim] %s (0x%zx)\n", name, player->get_field(name)->get_offset() );
   }
 
   Sleep( 60000 );
